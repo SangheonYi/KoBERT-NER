4500575fccadd6550cb43c9051305640ed78ffdf
SangheonYi <tmzpdpdlwl@gmail.com>
2022-04-18 11:18:18 +0900

    test: Test fixed dataset

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 rm -rf my_data/cache* model
-python3 main.py --model_type koelectra-base --do_train --data_dir ./my_data --save_steps 10 --num_train_epochs 15 --train_batch_size 64 --eval_batch_size 64
+python3 main.py --model_type koelectra-base --do_train --data_dir ./my_data --num_train_epochs 10 --train_batch_size 64 --eval_batch_size 64

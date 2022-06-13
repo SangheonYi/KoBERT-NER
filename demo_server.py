4b39b51dea5547beba36bd87424c0185875016f0
SangheonYi <tmzpdpdlwl@gmail.com>
2022-06-13 09:20:57 +0900

    test: Test pipeline time

--- demo_server.py
+++ demo_server.py
@@ -1,38 +1,42 @@
 from flask import Flask, request, jsonify
-
 from transformers import ElectraTokenizerFast, ElectraForTokenClassification
 from transformers import TokenClassificationPipeline
 import time
+
 app = Flask(__name__)
 pipeline = None
 
 def init_pipeline():
+    global pipeline
     # init config
     tokenizer = ElectraTokenizerFast.from_pretrained('monologg/koelectra-base-v3-discriminator')
     model = ElectraForTokenClassification.from_pretrained("model")
     pipeline = TokenClassificationPipeline(task="ner", model=model, tokenizer=tokenizer)
 
-    return pipeline
-
 @app.route('/pii_demo', methods=['POST'])
 def pii_demo():
+    global pipeline
     if pipeline == None:
         return "Server not ready"
     lines = request.get_json()["lines"]
     if not lines:
         return "Empty sentences requested"
     start = time.time()
-    pii_metas = pipeline(lines)
-    print(time.time() - start)
-    print(pii_metas)
-    json_data = jsonify({"result" : pii_metas})
-    # for line, result in zip(lines, pii_metas):
-    #     for meta in result:
-    #         line = line.replace(meta["token"], f'[{meta["token"]}:{meta["label"]}]', meta["start"])
-    # print(json_data.json)
+    sentence_metas = pipeline(lines)
+    print(time.time() - start, flush=True)
+
+    respo = [
+        [
+            {"label" : pii_metas["entity"],
+             'start': pii_metas['start'], 
+             'end': pii_metas['end']} 
+            for pii_metas in sentence_meta] 
+            for sentence_meta in sentence_metas
+        ]
+    json_data = jsonify({"result" : respo})
     return json_data
     
 if __name__ == '__main__':
-    pipeline = init_pipeline()
+    init_pipeline()
     app.config['JSON_AS_ASCII'] = False
     app.run(debug=True)

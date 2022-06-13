80f8da8ad7180853f72fdeaabd53e460b266da40
SangheonYi <tmzpdpdlwl@gmail.com>
2022-06-13 10:41:24 +0900

    feat: Feat print proccess momory usage

--- demo_server.py
+++ demo_server.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
 from transformers import ElectraTokenizerFast, ElectraForTokenClassification
 from transformers import TokenClassificationPipeline
+import torch
 import time
+import os, psutil
+process = psutil.Process(os.getpid())
 
 app = Flask(__name__)
 pipeline = None
@@ -21,22 +24,26 @@ def pii_demo():
     lines = request.get_json()["lines"]
     if not lines:
         return "Empty sentences requested"
-    start = time.time()
-    sentence_metas = pipeline(lines)
-    print(time.time() - start, flush=True)
+    with torch.no_grad():
+        # start = time.time()
+        sentence_metas = pipeline(lines)
+        # pipeline("dkkkkddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd")
+        # print(type(sentence_metas))
+        # print(time.time() - start, flush=True)
 
-    respo = [
-        [
-            {"label" : pii_metas["entity"],
-             'start': pii_metas['start'], 
-             'end': pii_metas['end']} 
-            for pii_metas in sentence_meta] 
-            for sentence_meta in sentence_metas
-        ]
-    json_data = jsonify({"result" : respo})
-    return json_data
+        # respo = [
+        #     [
+        #         {"label" : pii_metas["entity"],
+        #         'start': pii_metas['start'], 
+        #         'end': pii_metas['end']} 
+        #         for pii_metas in sentence_meta] 
+        #         for sentence_meta in sentence_metas
+        #     ]
+        # json_data = jsonify({"result" : respo})
+        print(len(lines), process.memory_info().rss / 1024 ** 2)
+        return "json_data"
     
 if __name__ == '__main__':
     init_pipeline()
     app.config['JSON_AS_ASCII'] = False
-    app.run(debug=True)
+    app.run(debug=False)

beecc17f6bd75adf176f2713166ee15ed5722a08
SangheonYi <tmzpdpdlwl@gmail.com>
2022-06-16 10:46:53 +0900

    test: Test memory leak

--- demo_server.py
+++ demo_server.py
@@ -4,9 +4,11 @@ from transformers import TokenClassificationPipeline
 import torch
 import time
 import os, psutil
+
 process = psutil.Process(os.getpid())
 count = 0
 spent_sum = 0
+start_memory = 0
 app = Flask(__name__)
 pipeline = None
 
@@ -15,13 +17,14 @@ def init_pipeline():
     # init config
     tokenizer = ElectraTokenizerFast.from_pretrained('monologg/koelectra-base-v3-discriminator')
     model = ElectraForTokenClassification.from_pretrained("model")
-    pipeline = TokenClassificationPipeline(task="ner", model=model, tokenizer=tokenizer)
+    pipeline = TokenClassificationPipeline(model=model, tokenizer=tokenizer, framework='pt')
 
 @app.route('/pii_demo', methods=['POST'])
 def pii_demo():
     global pipeline
     global spent_sum
     global count
+    global start_memory
     
     if pipeline == None:
         return "Server not ready"
@@ -32,23 +35,20 @@ def pii_demo():
     with torch.no_grad():
         count += 1
         start = time.time()
-        sentence_metas = pipeline(lines)
-        spent = time.time() - start 
+        pipeline(lines, batch_size=32)
+        spent = round(time.time() - start, 3)
         spent_sum += spent
         print("spent", spent)
         print("spent_avg", round(spent_sum / count, 2))
-
-        # respo = [
-        #     [
-        #         {"label" : pii_metas["entity"],
-        #         'start': pii_metas['start'], 
-        #         'end': pii_metas['end']} 
-        #         for pii_metas in sentence_meta] 
-        #         for sentence_meta in sentence_metas
-        #     ]
-        # json_data = jsonify({"result" : respo})
-        print("memory usages:", process.memory_info().rss / 1024 ** 2)
-        return "json_data"
+        memory = round(process.memory_info().rss / 1024 ** 2, 3)
+        if count == 1:
+            start_memory = memory
+        print("memory usages:", memory)
+        print("memory start, end, differencies:", start_memory, memory, round(memory - start_memory, 3))
+        return {
+            "memory": memory,
+            "spent": spent
+        }
     
 if __name__ == '__main__':
     init_pipeline()
